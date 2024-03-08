bb7ab5296246ba7de93453224a6eb3134a3acd4e
Ibrahim <ibrahim9826@outlook.com>
2024-03-08 20:02:50 +0100

    Further worked on autofocus

--- devices/image_based_autofocus.py
+++ devices/image_based_autofocus.py
@@ -4,28 +4,6 @@ Sci Rep 6, 28962 (2016). https://doi.org/10.1038/srep28962"""
 
 import numpy as np
 
-variables = {"Time to execute": None, "Memory requirement": None, "Accuracy estimator": None}
-
-
-def maximum_variance(img):
-    res = np.var(img)
-
-    variables["Variance"] = res
-
-    return res
-
-def brenner(img):
-    rows = img.shape[0]
-    columns = img.shape[1] - 2
-    temp = np.zeros((rows, columns))
-
-    temp[:] = ((img[:, 0:-2] - img[:, 2:]) ** 2)
-    res = temp.sum()
-
-    variables["Brenner"] = res
-
-    return res
-
 
 def calculate_power_spectrum(data, normalize=True):
     """
@@ -43,82 +21,133 @@ def calculate_power_spectrum(data, normalize=True):
     return power
 
 
-def calculate_summed_power(power, x_spacing=(1 / 0.01)):
-    """
-        Calculate a 1D power spectrum from 2D power spectrum, by summing all rows and
-        columns, and then summing negative and positive frequencies, to form a
-        N/2+1 long 1D array. This approach is significantly faster to calculate
-        than the radial average.
-        """
-
-    # Sum rows and columns
-    sum_ = np.sum([np.sum(power, axis=i) for i in power.shape])
-
-    # Using 0 center, add the negative Hz to the positive Hz
-    zero = np.floor([float(sum_.size) / 2])[0]
-    sum_[zero + 1:] = sum_[zero + 1:] + sum_[:zero - 1][::-1]
-    sum_ = sum_[zero:]
-
-    f_k = np.linspace(0, 1, sum_.size) * (1.0 / (2 * x_spacing))
-
-    variables["Total power"] = sum_
-    variables["Frequency"] = f_k
-
-    return [f_k, sum_]
-
-
 def calculate_percent_spectrum(simple_power):
     simple_power[1] /= (simple_power[1].sum() / 100)
     return simple_power[1]
 
 
-def spectral_moments(img):
-    """
-    Our implementation of the Spectral Moments autofocus metric
-    Firestone, L. et al (1991). Comparison of autofocus methods for automated
-    microscopy. Cytometry, 12(3), 195–206.
-    http://doi.org/10.1002/cyto.990120302
-    """
+class AutoFocus:
+    def __init__(self):
+        self.num = 0
+        self.variables = {"Time to execute": None, "Memory requirement": None, "Accuracy estimator": None}
+        self.metrics = ["Variance", "Brenner", "Spectral Moments", "Psm mean", "Psm std", "Psm meanbin",
+                        "Metric comparison"]
 
-    power = calculate_power_spectrum(img)
+    def Variance(self, img):
+        res = np.var(img)
+        key = "Variance"
+        self.variables["Metrics"] = key
+        self.variables[key] = res
+        self.variables["Img_ID"] = self.num
 
-    summed_power = calculate_summed_power(power, x_spacing=(1 / 0.01))
+        return res
 
-    percent_spectrum = calculate_percent_spectrum(summed_power)
+    def Brenner(self, img):
+        rows = img.shape[0]
+        columns = img.shape[1] - 2
+        temp = np.zeros((rows, columns))
 
-    bin_index = np.arange(1, percent_spectrum[1].shape[0] + 1)
+        temp[:] = ((img[:, 0:-2] - img[:, 2:]) ** 2)
+        res = temp.sum()
+        key = "Brenner"
+        self.variables["Metrics"] = key
+        self.variables[key] = res
+        self.variables["Img_ID"] = self.num
 
-    res = (percent_spectrum[1] * np.log10(bin_index)).sum()
+        return res
 
-    variables["Spectral Moments"] = res
+    def calculate_summed_power(self, power, x_spacing=(1 / 0.01)):
+        """
+            Calculate a 1D power spectrum from 2D power spectrum, by summing all rows and
+            columns, and then summing negative and positive frequencies, to form a
+            N/2+1 long 1D array. This approach is significantly faster to calculate
+            than the radial average.
+            """
 
-    return res
+        # Sum rows and columns
+        sum_ = np.sum([np.sum(power, axis=i) for i in power.shape])
 
+        # Using 0 center, add the negative Hz to the positive Hz
+        zero = np.floor([float(sum_.size) / 2])[0]
+        sum_[zero + 1:] = sum_[zero + 1:] + sum_[:zero - 1][::-1]
+        sum_ = sum_[zero:]
 
-def analyze_power_spectrum(img, power_threshold=0.02):
-    """
-    Run the image quality analysis on the power spectrum
-    """
+        f_k = np.linspace(0, 1, sum_.size) * (1.0 / (2 * x_spacing))
 
-    power = calculate_power_spectrum(img)
+        key = "Psw"
+        self.variables["Variables"] = key
 
-    summed_power = calculate_summed_power(power, x_spacing=(1 / 0.01))
+        self.variables["Total power"] = sum_
+        self.variables["Frequency"] = f_k
 
-    # Extract the power spectrum tail
-    hf_sum = summed_power[1][summed_power[0] > power_threshold * summed_power[0].max()]
+        self.variables["Img_ID"] = self.num
 
-    mean = np.mean(hf_sum)
-    std = np.std(hf_sum)
-    mean_bin = np.mean(hf_sum[0:5])
+        return [f_k, sum_]
 
-    variables["Psm mean"] = mean
-    variables["Psm std"] = std
-    variables["Psm meanbin"] = mean_bin
+    def Spectral_moments(self, img):
+        """
+        Our implementation of the Spectral Moments autofocus metric
+        Firestone, L. et al (1991). Comparison of autofocus methods for automated
+        microscopy. Cytometry, 12(3), 195–206.
+        http://doi.org/10.1002/cyto.990120302
+        """
 
-    return [mean, std, mean_bin]
+        power = calculate_power_spectrum(img)
 
+        summed_power = self.calculate_summed_power(power, x_spacing=(1 / 0.01))
 
-def combinatorial():
-    # Combine INvsStd and Entropy for noise and fine detail?
+        percent_spectrum = calculate_percent_spectrum(summed_power)
 
-    pass
+        bin_index = np.arange(1, percent_spectrum[1].shape[0] + 1)
+
+        res = (percent_spectrum[1] * np.log10(bin_index)).sum()
+
+        key = "Spectral Moments"
+        self.variables["Metrics"] = key
+        self.variables["Img_ID"] = self.num
+
+        self.variables[key] = res
+
+        return res
+
+    def power_spectrum(self, img, power_threshold=0.02):
+        power = calculate_power_spectrum(img)
+
+        summed_power = self.calculate_summed_power(power, x_spacing=(1 / 0.01))
+
+        # Extract the power spectrum tail
+        return summed_power[1][summed_power[0] > power_threshold * summed_power[0].max()]
+
+    def Psw_mean(self, img, power_threshold=0.02):
+        """
+        Run the image quality analysis on the power spectrum
+        """
+
+        mean = np.mean(self.power_spectrum(img, power_threshold=power_threshold))
+        key = "Psm mean"
+        self.variables["Metrics"] = key
+        self.variables[key] = mean
+        self.variables["Img_ID"] = self.num
+
+    def Psw_std(self, img, power_threshold=0.02):
+        """
+        Run the image quality analysis on the power spectrum
+        """
+
+        mean = np.std(self.power_spectrum(img, power_threshold=power_threshold))
+        key = "Psm std"
+        self.variables["Metrics"] = key
+        self.variables[key] = mean
+        self.variables["Img_ID"] = self.num
+
+    def Psw_meanbin(self, img, power_threshold=0.02):
+        mean_bin = np.mean(self.power_spectrum(img, power_threshold=power_threshold)[0:5])
+        key = "Psm meanbin"
+        self.variables["Metrics"] = key
+        self.variables[key] = mean_bin
+        self.variables["Img_ID"] = self.num
+
+    def combinatorial(self):
+        # Combine INvsStd and Entropy for noise and fine detail?
+
+        pass
--- gui/GUI_Protocol.py
+++ gui/GUI_Protocol.py
@@ -1,32 +1,59 @@
+import glob
+import logging
+import os
+import sys
+from time import sleep
+import numpy as np
+from PyQt6.QtWidgets import QGridLayout, QPushButton, QWidget, QLineEdit, QVBoxLayout, QComboBox, QHBoxLayout, \
+    QApplication
+from PyQt6.QtCore import Qt, QTimer
+from matplotlib import pyplot as plt
+from devices.micrscope import Microscope
+from devices.wellplate import WellPlate
+from devices.protocol import Protocol
+from devices.image_based_autofocus import AutoFocus
+from gui.helperfunctions import create_colored_label
 
 
-
-class GUIProtocol(Qwidget):
-    def __init__():
+class GUIProtocol(QWidget):
+    def __init__(self):
         super().__init__()
 
+        self.img = None
         self.img_name = None
-
+        self.autofocus = AutoFocus()
         z_stack_layout_0 = QVBoxLayout()
         self.z_increment = QLineEdit(parent=self)
         self.z_increment.setPlaceholderText("mm")
-        z_stack_layout.addWidget(self.z_increment)
+        z_stack_layout_0.addWidget(self.z_increment)
         self.n_aqcuisitions = QLineEdit(parent=self)
         self.n_aqcuisitions.setPlaceholderText("e.g. 5")
-        z_stack_layout.addWidget(self.n_aqcuisitions)
+        z_stack_layout_0.addWidget(self.n_aqcuisitions)
         self.autofocus_title = create_colored_label("Choose autofocus algorithm", self)
-        z_stack_layout.addWidget(self.autofocus_title)
+        z_stack_layout_0.addWidget(self.autofocus_title)
         self.dropdown = QComboBox(self)
-        options = ["Brenner", "P"]
-        for x in options:
+        for x in self.autofocus.metrics:
             self.dropdown.addItem(x)
-        
+        z_stack_layout_0.addWidget(self.dropdown)
 
-        
-        z_stack_layout_1 = QHBoxLayout()
+        z_stack_layout_2 = QHBoxLayout()
+
+        z_stack_layout_1 = QVBoxLayout()
         self.z_height_travelled = create_colored_label("Z position starts at {} and will end at {}, confirm that \n the objective will not crash into the well plate.".format(), self)
-        self.z_increment.setPlaceholderText("mm")
+        z_stack_layout_1.addWidget(self.z_height_travelled)
+        self.enter_button = QPushButton("Enter", parent=self)
+        z_stack_layout_1.addWidget(self.enter_button)
+        self.enter_button.clicked.connect(self.enter_button_click)
 
+        z_stack_layout_2.addLayout(z_stack_layout_0)
+        z_stack_layout_2.addLayout(z_stack_layout_1)
+        
+        self.setLayout(z_stack_layout_2)
+        
+    def run_automated_acquisition(self):
+        
+        if self.dropdown.currentText() == "Variance":
+            f = self.autofocus.maximum_variance
 
 
         
